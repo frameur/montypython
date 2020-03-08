c80d8a3b43e67604c8c0755d6bec5c9ad382df53
frameur <60637865+frameur@users.noreply.github.com>
2020-03-08 23:00:34 +0100

    mise carousel

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-var nom = 'francois'
+// const afficherOnglet = function(a){
+//     var navItem = document.querySelectorAll('.navbar-nav a')
+//     for (var i = 0; <navItem.length; i++)
+//         navItem[i].addEventListener('click',function(e){
+//     var li = a.parentNode
+//     var div = a.parentNode.parentNode.parentNode
+// if(li.classList.contains('active')){
+//     return false
+// }
+// //ont retirent onglet actif
 
-console.log(nom);
+// div.querySelector('.navbar-nav .active')
 
+// //ajout active actuel
+
+// li.classList.add('active')
+
+// div.querySelector('.nav-link .active').classList.remove('active')
+// div.querySelector(this.getAttribute('href')).classList.add('active')
+// })
+// }
