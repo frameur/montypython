1838213eada72a93bec7321dc7e9a03983369216
francoisramos6121@gmail.com <francoisramos6121@gmail.com>
2020-03-10 15:28:36 +0100

    time2

--- time.js
+++ time.js
@@ -1,6 +1,6 @@
 //le python temps
 //declaration variable
-const d = new Date()
+const d = new Date("Mars 10, 2020 01:15:00Z");
 
 
 
@@ -9,6 +9,7 @@ function refreshTime(){
 let tempsCourant = document.getElementById("timeactu"); 
     let d = new Date();
     tempsCourant.innerHTML = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    
 }
 refreshTime();
 let compteurTemps = setInterval(refreshTime, 1000);
@@ -23,5 +24,30 @@ document.getElementById("stop").addEventListener("click", function () {
 document.getElementById("start").addEventListener("click", function () {
     refreshTime();
     let compteurTemps = setInterval(refreshTime, 1000);
+    
+    
 })
+
+  
 // end le python temps
+
+//le compte a rebours
+
+// const timerElt = document.getElementById('rebours');
+// const counter = 100;
+
+// const timerElt = setInterval(function (){
+
+//     timerElt.innerText = counter;
+//     counter--;
+//     if (counter === 0){
+//         setTimeout(function(){
+//             clearInterval(rebours)
+//         }, 1000);
+//     }
+
+
+// })
+
+
+
