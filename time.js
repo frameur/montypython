4884d7c66832a2ae96296c8f400245bbb73b90d5
frameur <60637865+frameur@users.noreply.github.com>
2020-03-11 07:43:42 +0100

    modiffuc

--- time.js
+++ time.js
@@ -6,13 +6,14 @@ function refreshTime() {
     const tempsCourant = document.getElementById("timeactu");
     const d = new Date();
 
-//affichage heure 
+//condition ajout du zero
 function addZero(i){
     if(i< 10){
         i = "0" + i;
     }
     return i;
 }
+//affichage heure 
     tempsCourant.innerHTML = addZero (d.getHours()) + ":" + addZero (d.getMinutes()) + ":" + addZero (d.getSeconds());
 
 
