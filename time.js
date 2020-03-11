42cc6571cd52db663664d98f4d020e92dd4a5fd6
frameur <60637865+frameur@users.noreply.github.com>
2020-03-11 06:49:00 +0100

    condition zero

--- time.js
+++ time.js
@@ -1,12 +1,19 @@
 //le python temps
 
 
-//fonction de refresh
+//fonction pour eviter temps de latence  d'affichage 
 function refreshTime() {
     const tempsCourant = document.getElementById("timeactu");
     const d = new Date();
-    //affichage heure 
-    tempsCourant.innerHTML = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+
+//affichage heure 
+function addZero(i){
+    if(i< 10){
+        i = "0" + i;
+    }
+    return i;
+}
+    tempsCourant.innerHTML = addZero (d.getHours()) + ":" + addZero (d.getMinutes()) + ":" + addZero (d.getSeconds());
 
 
 }
@@ -14,6 +21,7 @@ function refreshTime() {
 
 refreshTime();
 let compteurTemps = setInterval(refreshTime, 1000);
+
 //commande arret heure
 document.getElementById("stop").addEventListener("click", function () {
     clearInterval(compteurTemps);
@@ -28,5 +36,4 @@ document.getElementById("start").addEventListener("click", function () {
 
 
 });
-
 
