b7001cf7e05f27a76e246c15a12d32d6a050cbb9
frameur <francoisramos6121@gmail.com>
2022-07-02 21:33:31 +0200

    border radius navitoggle

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,30 @@
-import React from "react";
-import Darkmode from "../components/Darkmode";
-import Footer from "../components/Footer";
-import Navigation from "../components/Navigation";
-import Slider from "../components/Slider";
-import Time1 from "../components/Time1";
-import Time2 from "../components/Time2";
+import React from 'react'
+import Darkmode from '../components/Darkmode'
+import Footer from '../components/Footer'
+import Navigation from '../components/Navigation'
+import Slider from '../components/Slider'
+import Time1 from '../components/Time1'
+import Time2 from '../components/Time2'
 
 const Home = () => {
   return (
     <div>
-      
       <Navigation />
-      <section><Darkmode />
-        <Time1 />
-        <Time2 />
-        <Slider />
+      <section className="content">
+        <Darkmode />
+        <div className="time1">
+          <Time1 />
+        </div>
+        <div className="time2">
+          <Time2 />
+        </div>
+        <div className="slider">
+          <Slider />
+        </div>
       </section>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
