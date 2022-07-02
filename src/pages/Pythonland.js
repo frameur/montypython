be447bbf2e5c270572f10b46d305d6115c7c7694
frameur <francoisramos6121@gmail.com>
2022-07-02 21:57:54 +0200

    overflow page pythonland

--- src/pages/Pythonland.js
+++ src/pages/Pythonland.js
@@ -1,20 +1,20 @@
-import React from "react";
-import Darkmode from "../components/Darkmode";
-import Form from "../components/Form";
-import Headerland from "../components/Headerland";
-import Navigation from "../components/Navigation";
+import React from 'react'
+import Darkmode from '../components/Darkmode'
+import Form from '../components/Form'
+import Headerland from '../components/Headerland'
+import Navigation from '../components/Navigation'
 
 const Pythonland = () => {
   return (
     <div>
       <Darkmode />
       <Navigation />
-      <section>
+      <section className="moviescard">
         <Headerland />
         <Form />
       </section>
     </div>
-  );
-};
+  )
+}
 
-export default Pythonland;
+export default Pythonland
